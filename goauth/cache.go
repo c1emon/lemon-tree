bfeb1e5e20fc489d66695d20abde2c34be77eb01
clemon <cjw7360chen@gmail.com>
2022-12-04 23:20:06 +0800

    impl InMemoryStateCache

--- goauth/cache.go
+++ goauth/cache.go
@@ -1,30 +1,69 @@
 package goauth
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type StateCache interface {
-	Get(string) string
+	Get(string) (string, error)
 	Set(string, string, int64)
 }
 
 type State struct {
-	Value     string
+	ClientId  string
 	ExpiredAt int64
 }
 
 type InMemoryStateCache struct {
-	cache map[string]State
+	cachedStates map[string]State
+	ticker       *time.Ticker
 }
 
-func (c *InMemoryStateCache) Get(clientId string) string {
-	time.Now().Unix()
-
-	return c.cache[clientId].Value
+func NewInMemoryStateCache() *InMemoryStateCache {
+	c := &InMemoryStateCache{
+		cachedStates: make(map[string]State),
+		ticker:       time.NewTicker(time.Duration(20)),
+	}
+	c.Start()
+	return c
 }
 
-func (c *InMemoryStateCache) Set(clientId, state string, timeOut int64) {
-	c.cache[clientId] = State{
-		Value:     state,
+func (c *InMemoryStateCache) Start() {
+	go func() {
+		for {
+			select {
+			case <-c.ticker.C:
+				c.timeoutChecker()
+			}
+		}
+	}()
+}
+
+func (c *InMemoryStateCache) Stop() {
+	c.ticker.Stop()
+}
+
+func (c *InMemoryStateCache) timeoutChecker() {
+	now := time.Now().Unix()
+	for k, v := range c.cachedStates {
+		if v.ExpiredAt >= now {
+			delete(c.cachedStates, k)
+		}
+	}
+}
+
+func (c *InMemoryStateCache) Get(state string) (string, error) {
+	s, ok := c.cachedStates[state]
+	if !ok {
+		return "", errors.New("invalid state")
+	}
+	return s.ClientId, nil
+}
+
+func (c *InMemoryStateCache) Set(state, clientId string, timeOut int64) {
+	c.cachedStates[state] = State{
+		ClientId:  clientId,
 		ExpiredAt: time.Now().Unix() + timeOut,
 	}
 }
