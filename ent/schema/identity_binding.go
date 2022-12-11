2453d06b3061876712572b27782e18b37443ac4d
clemon <cjw7360chen@gmail.com>
2022-12-11 17:08:51 +0800

    update ignore

---
+++ ent/schema/identity_binding.go
@@ -0,0 +1,43 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// IdentityBinding holds the schema definition for the IdentityBinding entity.
+type IdentityBinding struct {
+	ent.Schema
+}
+
+// Fields of the IdentityBinding.
+func (IdentityBinding) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("identity_id"),
+		field.String("meta_data_json"),
+	}
+}
+
+// Edges of the IdentityBinding.
+func (IdentityBinding) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("organization", Organization.Type).
+			Ref("identity_bindings").
+			Unique(),
+		edge.From("identity_provider", IdentityProvider.Type).
+			Ref("identity_bindings").
+			Unique(),
+		edge.From("staff", Staff.Type).
+			Ref("identity_bindings").
+			Unique(),
+	}
+}
+
+func (IdentityBinding) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		CommonMixin{},
+		// Or, mixin.CreateTime only for create_time
+		// and mixin.UpdateTime only for update_time.
+	}
+}
---
+++ ent/schema/mixin.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+	"github.com/lucsky/cuid"
+	"time"
+)
+
+type CommonMixin struct {
+	// We embed the `mixin.Schema` to avoid
+	// implementing the rest of the methods.
+	mixin.Schema
+}
+
+func (CommonMixin) Fields() []ent.Field {
+	return []ent.Field{
+		field.Time("created_at").
+			Immutable().
+			Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+		field.String("external_id").
+			Unique().Immutable().NotEmpty().
+			DefaultFunc(cuid.New),
+	}
+}
