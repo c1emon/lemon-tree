423cf2776552bfc14d6e62d1bf1407e498abe678
clemon <cjw7360chen@gmail.com>
2023-01-04 21:04:39 +0800

    init test

--- test/db_test.go
+++ test/db_test.go
@@ -11,15 +11,10 @@ import (
 
 func Test_DbCreate(t *testing.T) {
 	config.SetConfig(8080, "postgres", "host=10.0.0.70 port=5432 user=postgres dbname=lemon_tree password=123456 sslmode=disable")
-
-	defer func() {
-		if err := persister.DisConnect(); err != nil {
-			log.GetLogger().Warnf("unable close db: %s", err)
-		}
-	}()
+	log.Init("trace")
 
 	r := persister.NewDefaultOrganizationRepository()
-	r.CreateOne(context.Background(), &model.Organization{Name: "ta2aest"})
-	r.CreateOne(context.Background(), &model.Organization{Name: "taaest"})
+	r.CreateOne(context.Background(), &model.Organization{Name: "ta2asaest"})
+	//r.CreateOne(context.Background(), &model.Organization{Name: "taaest"})
 
 }
